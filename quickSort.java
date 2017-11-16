b1b4d112d3e6ee9cee14a61057bdfa1d0eaa3970
wangyushuang <wangyushuang_guyan@126.com>
2017-11-16 15:49:55 +0800

    快速排序

--- quickSort.java
+++ quickSort.java
@@ -7,7 +7,7 @@ public class quickSort{
 		System.out.println(Arrays.toString(A));
 	}
 	public static void sort(int[] A){
-		sortCore(A,0,A.length-1);
+		sortCore1(A,0,A.length-1);
 	}
 	private static void sortCore(int[] A, int left, int right){
 		//将比pivot小的数放在pivot的左边，比pivot大的放右边
@@ -26,6 +26,26 @@ public class quickSort{
 			A[i]=pivot;
 			sortCore(A,left,i-1);
 			sortCore(A,i+1,right);
+		}
+	}
+	private static void sortCore1(int[] A,int left, int right){
+		if(left<right){
+			int pivot=A[left];
+			int i=left;//位置i前面的数都小于pivot
+			int tmp;
+			for(int j=left+1;j<=right;j++){
+				if(A[j]<pivot){//如果位置j的元素小于pivot，则swap(A[i],A[j])
+					i++;
+					tmp=A[i];
+					A[i]=A[j];
+					A[j]=tmp;
+				}
+			}
+			//swap(A[i],pivot)
+			A[left]=A[i];
+			A[i]=pivot;
+			sortCore1(A,left,i-1);
+			sortCore1(A,i+1,right);
 		}
 	}
 }
