bd39c891893b803f978850813324b4248325408c
wangyushuang <wangyushuang_guyan@126.com>
2017-11-16 11:21:59 +0800

    堆排序

--- heapSort.java
+++ heapSort.java
@@ -1,6 +1,4 @@
 //堆排序(heapsort)
-//注意：堆标号从1开始，数组标号从0开始
-//		所以，A[i-1]表示堆的第i个元素
 import java.util.Arrays;
 public class heapSort{
 	public static void main(String[] args){
@@ -10,33 +8,33 @@ public class heapSort{
 	}
 	public static void sort(int [] A){
 		//build max heap  建堆
-		for(int i=(A.length>>1);i>=1;i--)
+		for(int i=(A.length>>1)-1;i>=0;i--)
 			//堆的第一个非叶子结点的标号为：A.length/2
-			max_heapify(A,i,A.length);
+			max_heapify(A,i,A.length-1);
 		//heapsort 堆排序
 		int tmp;
-		for(int i=A.length;i>1;i--){
+		for(int i=A.length-1;i>0;i--){
 			//交换堆头和堆尾元素，并将堆的长度减一
 			tmp=A[0];
-			A[0]=A[i-1];
-			A[i-1]=tmp;
-			max_heapify(A,1,i-1); 
+			A[0]=A[i];
+			A[i]=tmp;
+			max_heapify(A,0,i-1); 
 		}
 	}
 	private static void max_heapify(int[] A, int i, int heapSize){
 		//heapSize: 0-A.length-1
-		int l=2*i;
-		int r=2*i+1;
+		int l=2*i+1;
+		int r=2*i+2;
 		int largest=i;
-		if(l<=heapSize && A[l-1]>A[i-1])
+		if(l<=heapSize && A[l]>A[i])
 			largest=l;
-		if(r<=heapSize && A[r-1]>A[largest-1])
+		if(r<=heapSize && A[r]>A[largest])
 			largest=r;
 		if(largest!=i){
 			//交换堆中第i个元素和第largest个元素
-			int tmp=A[i-1];
-			A[i-1]=A[largest-1];
-			A[largest-1]=tmp;
+			int tmp=A[i];
+			A[i]=A[largest];
+			A[largest]=tmp;
 			max_heapify(A,largest,heapSize);
 		}
 
